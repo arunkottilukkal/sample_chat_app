726a1ae72627331deda711ee795dbf2f4529f0c7
Arun Kesavan <kottilukkal.5@apac.teleperformance.com>
2023-04-10 20:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import requests
 import json
 from flask import Flask, request, render_template, session, url_for, jsonify
 from flask_wtf.csrf import CSRFProtect
-
+import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-app.secret_key = 'my-secret-key'
+app.secret_key = os.environ.get("APP_SECRET", "4TM2NBktihlGzCTRQqLAYooSs3QD_h_fLGmoq2sROj4sNgV8i7KROHxu4RdNsbFUy3k")
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['WTF_CSRF_CHECK_DEFAULT'] = False
 
 url = "https://seal-app-aafp5.ondigitalocean.app/query"
 headers = {
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsIkZ1bGxOYW1lIjoiS290dGlsdWtrYWwgQXJ1biBLZXNhdmFuIiwiQ29udHJhY3RFbWFpbCI6Ind3dy5sb2NhbGhvc3Q4MDg2LmNvbUBnbWFpbC5jb20iLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.uHSJPxwGWOfCPyQ8Y0cBw7UF2VfWYv3KrHDl-adlPFA',
+    'Authorization': 'Bearer ' + os.environ.get("BEARER_TOKEN"),
     'Content-Type': 'application/json'
 }
 
@@ -22,26 +22,27 @@ headers = {
 def home():
     return render_template("index.html")
 
-@app.post("/searchResult")
+@app.route("/searchResult", methods=["POST"])
 def searchResult():
-    query = request.form['query']
+    if request.method == "POST":
+        query = request.form['query']
 
-    payload = json.dumps({
-    "queries": [
-        {
-            "query": query
+        payload = json.dumps({
+        "queries": [
+            {
+                "query": query
+            }
+        ]
+        })
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        
+        data = json.loads(response.text)
+
+        context = {
+            "query": query,
+            "results":data["results"][0]["results"]
         }
-    ]
-    })
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-    
-    data = json.loads(response.text)
-
-    context = {
-        "query": query,
-        "results":data["results"][0]["results"]
-    }
 
 
-    return render_template("search_results.html", context=context)
+        return render_template("search_results.html", context=context)
